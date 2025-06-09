d9bc6e53ba9cea1389573e9433a28f14831473b2
Whatdoyumin <lym4963@naver.com>
2025-06-09 11:47:34 +0900

    [1일차] 의존성 주입 테스트

--- ex02/src/main/java/org/scoula/dynamicweb/config/RootConfig.java
+++ ex02/src/main/java/org/scoula/dynamicweb/config/RootConfig.java
@@ -1,7 +1,10 @@
 package org.scoula.dynamicweb.config;
 
+import org.scoula.Restaurant;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"org.scoula"})
 public class RootConfig {
 }
