f00df6acb565054e90d7d6f0ccf08ee693452d80
Whatdoyumin <lym4963@naver.com>
2025-06-09 11:49:28 +0900

    [1일차] Autowired에서 생성자 의존으로 변경 (권장 방법)

--- ex02/src/main/java/org/scoula/Restaurant.java
+++ ex02/src/main/java/org/scoula/Restaurant.java
@@ -1,12 +1,13 @@
 package org.scoula;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@RequiredArgsConstructor
 public class Restaurant {
-    @Autowired
-    private Chef chef;
+    private final Chef chef;
 }
