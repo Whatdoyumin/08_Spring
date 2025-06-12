55398175669aad7d2f90e188a89b4bcad55daddd
Whatdoyumin <lym4963@naver.com>
2025-06-12 10:45:39 +0900

    [4일차] 영속 계층 CRUD 구현

--- board/src/main/java/org/scoula/config/RootConfig.java
+++ board/src/main/java/org/scoula/config/RootConfig.java
@@ -17,7 +17,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
-// @MapperScan(basePackages = {""})
+@MapperScan(basePackages = {"org.scoula.board.mapper"})
 public class RootConfig {
     @Value("${jdbc.driver}") String driver;
     @Value("${jdbc.url}") String url;
