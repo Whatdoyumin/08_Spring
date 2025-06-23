b5afb6f265dfe92ebe3f9b9316f9125629af20fd
Whatdoyumin <lym4963@naver.com>
2025-06-23 10:27:30 +0900

    [8일차] 계정 유형별 security 제어 구현

--- secserver/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ secserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -5,13 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 문자셋 필터
+    public CharacterEncodingFilter encodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
+
+        // 경로별 접근 권한 설정
+        http.authorizeRequests()
+                .antMatchers("/security/all").permitAll()
+                .antMatchers("/security/admin").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/security/member").access("hasAnyRole('ROLE_MEMBER', 'ROLE_ADMIN')");
+
+        http.formLogin();
     }
 }
